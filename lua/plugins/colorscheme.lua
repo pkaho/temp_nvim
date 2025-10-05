ac4994f3b4ccb5aa4955108fa3e18d936c2bf031
pkaho <pkaho@qq.com>
2025-10-05 09:27:20 +0800

    add two themes, everforest and ayu

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,90 +1,115 @@
 return {
-	-- theme 选择器
-	{
-		"zaldih/themery.nvim",
-		lazy = false,
-		keys = {
-			{ "<leader>uc", "<cmd>Themery<cr>", desc = "Select Color Theme" },
-		},
-		opts = {
+    -- theme 选择器
+    {
+        "zaldih/themery.nvim",
+        lazy = false,
+        keys = {
+            { "<leader>uc", "<cmd>Themery<cr>", desc = "Select Color Theme" },
+        },
+        opts = {
             -- stylua: ignore
-			themes = {
-				-- { name = "----------------- Light -----------------" },
-				{ name = "catppuccin-latte",       colorscheme = "catppuccin-latte" },
-				{ name = "catppuccin-mocha",       colorscheme = "catppuccin-mocha" },
-				{ name = "catppuccin-frappe",      colorscheme = "catppuccin-frappe" },
-				{ name = "catppuccin-macchiato",   colorscheme = "catppuccin-macchiato" },
-				{ name = "cyberdream-dark",        colorscheme = "cyberdream" },
-				{ name = "cyberdream-light",       colorscheme = "cyberdream-light" },
-				{ name = "rose-pine-dawn",         colorscheme = "rose-pine-dawn" },
-				{ name = "rose-pine-moon",         colorscheme = "rose-pine-moon" },
-				{ name = "rose-pine-main",         colorscheme = "rose-pine-main" },
-				{ name = "flexoki-dark",           colorscheme = "flexoki-dark" },
-				{ name = "flexoki-light",          colorscheme = "flexoki-light" },
-				{ name = "nord",                   colorscheme = "nord" },    -- 无其他模式
-				{ name = "melange",                colorscheme = "melange" }, -- 跟随终端背景自动调节模式
-				{ name = "synthweave",             colorscheme = "synthweave" },
-				{ name = "synthweave-transparent", colorscheme = "synthweave-transparent" },
-			},
-			livePreview = true,
-		},
-	},
+            themes = {
+                -- { name = "----------------- Light -----------------" },
+                { name = "ayu",                    colorscheme = "ayu" },
+                { name = "ayu-dark",               colorscheme = "ayu-dark" },
+                { name = "ayu-light",              colorscheme = "ayu-light" },
+                { name = "ayu-mirage",             colorscheme = "ayu-mirage" },
+                { name = "catppuccin-frappe",      colorscheme = "catppuccin-frappe" },
+                { name = "catppuccin-latte",       colorscheme = "catppuccin-latte" },
+                { name = "catppuccin-macchiato",   colorscheme = "catppuccin-macchiato" },
+                { name = "catppuccin-mocha",       colorscheme = "catppuccin-mocha" },
+                { name = "cyberdream-dark",        colorscheme = "cyberdream" },
+                { name = "cyberdream-light",       colorscheme = "cyberdream-light" },
+                { name = "everforest",             colorscheme = "everforest" },
+                { name = "flexoki-dark",           colorscheme = "flexoki-dark" },
+                { name = "flexoki-light",          colorscheme = "flexoki-light" },
+                { name = "melange",                colorscheme = "melange" }, -- 跟随终端背景自动调节模式
+                { name = "nord",                   colorscheme = "nord" },    -- 无其他模式
+                { name = "rose-pine-dawn",         colorscheme = "rose-pine-dawn" },
+                { name = "rose-pine-main",         colorscheme = "rose-pine-main" },
+                { name = "rose-pine-moon",         colorscheme = "rose-pine-moon" },
+                { name = "synthweave",             colorscheme = "synthweave" },
+                { name = "synthweave-transparent", colorscheme = "synthweave-transparent" },
+            },
+            livePreview = true,
+        },
+    },
 
-	-- rose-pine
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-		config = function()
-			vim.cmd("colorscheme rose-pine")
-		end,
-	},
+    -- rose-pine
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end,
+    },
 
-	-- cyberdream
-	{
-		"scottmckendry/cyberdream.nvim",
-		lazy = true,
-		dependencies = {
-			"mawkler/modicator.nvim",
-			enabled = true,
-		},
-		priority = 1000,
-		opts = {
-			transparent = false,
-			italic_comments = true,
-			variant = "auto", -- default, auto, light
-		},
-	},
+    -- cyberdream
+    {
+        "scottmckendry/cyberdream.nvim",
+        dependencies = {
+            "mawkler/modicator.nvim",
+            enabled = true,
+        },
+        event = "VeryLazy",
+        opts = {
+            transparent = false,
+            italic_comments = true,
+            variant = "auto", -- default, auto, light
+        },
+    },
 
-	-- catppuccin
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		opts = {},
-	},
+    -- catppuccin
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        event = "VeryLazy",
+        opts = {},
+    },
 
-	-- flexoki
-	{
-		"kepano/flexoki-neovim",
-		event = "VeryLazy",
-		name = "flexoki",
-	},
+    -- flexoki
+    {
+        "kepano/flexoki-neovim",
+        name = "flexoki",
+        event = "VeryLazy",
+    },
 
-	-- nord
-	{
-		"shaunsingh/nord.nvim",
-		event = "VeryLazy",
-	},
+    -- nord
+    {
+        "shaunsingh/nord.nvim",
+        event = "VeryLazy",
+    },
 
-	-- melange
-	{
-		"savq/melange-nvim",
-	},
+    -- melange
+    {
+        "savq/melange-nvim",
+        event = "VeryLazy",
+    },
 
-	{
-		"samharju/synthweave.nvim",
-	},
+    -- ayu
+    {
+        "Shatur/neovim-ayu",
+        event = "VeryLazy",
+    },
+
+    -- everforest
+    {
+        "neanias/everforest-nvim",
+        event = "VeryLazy",
+        config = function()
+            require("everforest").setup({
+                background = "medium", -- soft, medium, hard
+                italics = true,        -- 斜体关键字
+            })
+        end
+    },
+
+    -- synthweave
+    {
+        "samharju/synthweave.nvim",
+        event = "VeryLazy",
+    },
 }
