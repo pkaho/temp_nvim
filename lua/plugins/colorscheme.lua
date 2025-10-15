5d37b0b3ecf28969f094d68439eb68616e97bd53
pkaho <pkaho@qq.com>
2025-10-15 17:56:30 +0800

    del some theme, add some theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,115 +1,76 @@
 return {
-    -- theme 选择器
-    {
-        "zaldih/themery.nvim",
-        lazy = false,
-        keys = {
-            { "<leader>uc", "<cmd>Themery<cr>", desc = "Select Color Theme" },
-        },
-        opts = {
-            -- stylua: ignore
-            themes = {
-                -- { name = "----------------- Light -----------------" },
-                { name = "ayu",                    colorscheme = "ayu" },
-                { name = "ayu-dark",               colorscheme = "ayu-dark" },
-                { name = "ayu-light",              colorscheme = "ayu-light" },
-                { name = "ayu-mirage",             colorscheme = "ayu-mirage" },
-                { name = "catppuccin-frappe",      colorscheme = "catppuccin-frappe" },
-                { name = "catppuccin-latte",       colorscheme = "catppuccin-latte" },
-                { name = "catppuccin-macchiato",   colorscheme = "catppuccin-macchiato" },
-                { name = "catppuccin-mocha",       colorscheme = "catppuccin-mocha" },
-                { name = "cyberdream-dark",        colorscheme = "cyberdream" },
-                { name = "cyberdream-light",       colorscheme = "cyberdream-light" },
-                { name = "everforest",             colorscheme = "everforest" },
-                { name = "flexoki-dark",           colorscheme = "flexoki-dark" },
-                { name = "flexoki-light",          colorscheme = "flexoki-light" },
-                { name = "melange",                colorscheme = "melange" }, -- 跟随终端背景自动调节模式
-                { name = "nord",                   colorscheme = "nord" },    -- 无其他模式
-                { name = "rose-pine-dawn",         colorscheme = "rose-pine-dawn" },
-                { name = "rose-pine-main",         colorscheme = "rose-pine-main" },
-                { name = "rose-pine-moon",         colorscheme = "rose-pine-moon" },
-                { name = "synthweave",             colorscheme = "synthweave" },
-                { name = "synthweave-transparent", colorscheme = "synthweave-transparent" },
-            },
-            livePreview = true,
-        },
-    },
+	-- 主题选择器
+	{
+		"zaldih/themery.nvim",
+		lazy = false, -- 必须 eager 加载, 否则 <leader>uc 无法使用
+		keys = {
+			{ "<leader>uc", "<cmd>Themery<cr>", desc = "Select Color Theme" },
+		},
+		dependencies = {
+			-- 所有支持的主题插件作为依赖
+			"Shatur/neovim-ayu",
+			"catppuccin/nvim",
+			"datsfilipe/vesper.nvim",
+			"kepano/flexoki-neovim",
+			"mellow-theme/mellow.nvim",
+			"neanias/everforest-nvim",
+			"nyoom-engineering/oxocarbon.nvim",
+			"rose-pine/neovim",
+			"sainnhe/sonokai",
+			"savq/melange-nvim",
+			"Yazeed1s/oh-lucy.nvim",
+		},
+		priority = 1000,
+		opts = {
+			livePreview = true,
+			-- stylua:ignore
+			themes = {
+				-- Light Themes
+				{ name = "ayu-light", colorscheme = "ayu-light" },
+				{ name = "catppuccin-latte", colorscheme = "catppuccin-latte" },
+				{ name = "flexoki-light", colorscheme = "flexoki-light" },
+				{ name = "oxocarbon", colorscheme = "oxocarbon", before = [[vim.o.background='light']] },
+				{ name = "rose-pine-dawn", colorscheme = "rose-pine-dawn" },
 
-    -- rose-pine
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-        config = function()
-            vim.cmd("colorscheme rose-pine")
-        end,
-    },
+				-- Dark Themes
+				{ name = "ayu-dark", colorscheme = "ayu-dark" },
+				{ name = "ayu-mirage", colorscheme = "ayu-mirage" },
+				{ name = "catppuccin-frappe", colorscheme = "catppuccin-frappe" },
+				{ name = "catppuccin-macchiato", colorscheme = "catppuccin-macchiato" },
+				{ name = "catppuccin-mocha", colorscheme = "catppuccin-mocha" },
+				{ name = "everforest", colorscheme = "everforest" },
+				{ name = "flexoki-dark", colorscheme = "flexoki-dark" },
+				{ name = "melange", colorscheme = "melange" },
+				{ name = "mellow", colorscheme = "mellow" },
+				{ name = "oh-lucy", colorscheme = "oh-lucy" },
+				{ name = "oh-lucy-evening", colorscheme = "oh-lucy-evening" },
+				{ name = "oxocarbon", colorscheme = "oxocarbon" },
+				{ name = "rose-pine-main", colorscheme = "rose-pine-main" },
+				{ name = "rose-pine-moon", colorscheme = "rose-pine-moon" },
+				{ name = "sonokai-andromeda", colorscheme = "sonokai", before = [[vim.g.sonokai_style='andromeda']] },
+				{ name = "sonokai-atlantis", colorscheme = "sonokai", before = [[vim.g.sonokai_style='atlantis']] },
+				{ name = "sonokai-default", colorscheme = "sonokai", before = [[vim.g.sonokai_style='default']] },
+				{ name = "sonokai-espresso", colorscheme = "sonokai", before = [[vim.g.sonokai_style='espresso']] },
+				{ name = "sonokai-maia", colorscheme = "sonokai", before = [[vim.g.sonokai_style='maia']] },
+				{ name = "sonokai-shusia", colorscheme = "sonokai", before = [[vim.g.sonokai_style='shusia']] },
+				{ name = "vesper", colorscheme = "vesper" },
+			},
+		},
+	},
 
-    -- cyberdream
-    {
-        "scottmckendry/cyberdream.nvim",
-        dependencies = {
-            "mawkler/modicator.nvim",
-            enabled = true,
-        },
-        event = "VeryLazy",
-        opts = {
-            transparent = false,
-            italic_comments = true,
-            variant = "auto", -- default, auto, light
-        },
-    },
+	-- 各主题插件: 仅加载, 不启用 colorscheme
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = true,
+		opts = {
+			flavour = "mocha", -- 默认值, 实际由 themery 切换时决定
+		},
+	},
 
-    -- catppuccin
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        event = "VeryLazy",
-        opts = {},
-    },
-
-    -- flexoki
-    {
-        "kepano/flexoki-neovim",
-        name = "flexoki",
-        event = "VeryLazy",
-    },
-
-    -- nord
-    {
-        "shaunsingh/nord.nvim",
-        event = "VeryLazy",
-    },
-
-    -- melange
-    {
-        "savq/melange-nvim",
-        event = "VeryLazy",
-    },
-
-    -- ayu
-    {
-        "Shatur/neovim-ayu",
-        event = "VeryLazy",
-    },
-
-    -- everforest
-    {
-        "neanias/everforest-nvim",
-        event = "VeryLazy",
-        config = function()
-            require("everforest").setup({
-                background = "medium", -- soft, medium, hard
-                italics = true,        -- 斜体关键字
-            })
-        end
-    },
-
-    -- synthweave
-    {
-        "samharju/synthweave.nvim",
-        event = "VeryLazy",
-    },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = true,
+	},
 }
