c45b2353a419284e211ab57f9ac4dccbfba0aa6e
pkaho <pkaho@qq.com>
2025-12-04 14:05:18 +0800

    add comment-box plug

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,57 +1,67 @@
 return {
-	-- 括号
-	{
-		"altermo/ultimate-autopair.nvim",
-		event = { "InsertEnter", "CmdlineEnter" },
-		opts = {
-			pair_cmap = false, -- 命令行补全
-		},
-	},
+    -- 括号
+    {
+        "altermo/ultimate-autopair.nvim",
+        event = { "InsertEnter", "CmdlineEnter" },
+        opts = {
+            pair_cmap = false, -- 命令行补全
+        },
+    },
 
-	-- 交互式对齐文本
-	{
-		"echasnovski/mini.align",
-		version = false,
-		opts = {},
-	},
+    -- 交互式对齐文本
+    {
+        "echasnovski/mini.align",
+        version = false,
+        opts = {},
+    },
 
-	-- 扩展并创建a/i文本对象
-	{
-		"echasnovski/mini.ai",
-		version = false,
-		opts = {},
-	},
+    -- 扩展并创建a/i文本对象
+    {
+        "echasnovski/mini.ai",
+        version = false,
+        opts = {},
+    },
 
-	-- 环绕操作(增删改查/高亮括号、引号等)
-	{
-		"echasnovski/mini.surround",
-		version = false,
-		opts = {
-			mappings = {
-				add = "gsa",
-				delete = "gsd",
-				find = "gsf",
-				find_left = "gsF",
-				highlight = "gsh",
-				replace = "gsr",
-				update_n_lines = "gsn",
+    -- 环绕操作(增删改查/高亮括号、引号等)
+    {
+        "echasnovski/mini.surround",
+        version = false,
+        opts = {
+            mappings = {
+                add = "gsa",
+                delete = "gsd",
+                find = "gsf",
+                find_left = "gsF",
+                highlight = "gsh",
+                replace = "gsr",
+                update_n_lines = "gsn",
 
-				suffix_last = "l",
-				suffix_next = "n",
-			},
-		},
-	},
+                suffix_last = "l",
+                suffix_next = "n",
+            },
+        },
+    },
 
-	-- 拆分/合并代码块(数组、哈希、语句、对象等)
-	{
-		"Wansmer/treesj",
-		event = "VeryLazy",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		keys = {
-			{ "<leader>cj", "<CMD>TSJToggle<CR>", desc = "Treesj Toggle" },
-		},
-		opts = {
-			use_default_keymaps = false,
-		},
-	},
+    -- 拆分/合并代码块(数组、哈希、语句、对象等)
+    {
+        "Wansmer/treesj",
+        event = "VeryLazy",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        keys = {
+            { "<leader>cj", "<cmd>TSJToggle<CR>", desc = "Treesj Toggle" },
+        },
+        opts = {
+            use_default_keymaps = false,
+        },
+    },
+
+    -- 美化注释
+    {
+        "LudoPinelli/comment-box.nvim",
+        event = "VeryLazy",
+        keys = {
+            { "<leader>Cbl", "<cmd>CBllbox<CR>", desc = "Comment Box Left" },
+        },
+        opts = {}
+    }
 }
